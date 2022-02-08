14962b4fc50a18d1d120524af8ddcbc9dacc8ce0
MrVipe <mr.vi@mail.ru>
2022-02-08 23:38:05 +0400

    Задание 4

--- Less4/Program.cs
+++ Less4/Program.cs
@@ -7,33 +7,26 @@ namespace Less4
         static void Main(string[] args)
         {
 
-            Console.WriteLine(ConvertStrToSummInt("5 10 55 43 1 3 10"));
-            Console.WriteLine(ConvertStrToSummInt("7 8 1 77 16 4 3"));
-            Console.WriteLine(ConvertStrToSummInt("1 2 3 4 5 6 7"));
+
+            Console.WriteLine("Порог расчета: ");
+            int isStop = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Число Фибоначи: " + gotoFibon(0, isStop, 0, 1));
             Console.ReadKey();
         }
 
-        static double ConvertStrToSummInt(string strNumber) 
+        static double gotoFibon(int isBegin, int isStop, Double numFirst, Double numSecond) 
         {
-            double summNumber = 0;
-            string numPlus = "";
 
-            for (int i = 0; i < strNumber.Length; i++)
+            if (isBegin < isStop)
             {
-                
-                if (Char.IsNumber(strNumber[i]))
-                {
-                    numPlus += strNumber[i];
-                    if (i == strNumber.Length - 1) summNumber += Convert.ToDouble(numPlus);
-                }
-                else
-                {
-                    summNumber += Convert.ToDouble(numPlus);
-                    numPlus = "";
-                }    
-
+                isBegin++;
+                return gotoFibon(isBegin, isStop, numSecond, numFirst + numSecond);
             }
-            return summNumber;
+            else
+            {
+                return numSecond;
+            }
+          
         }
          
     }
